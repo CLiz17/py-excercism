f7e5297c6f28b0ab388de5ae5090ab8bf1f5932f
CLiz17 <liz362002@gmail.com>
2024-05-14 16:39:59 +0530

    Add func, var and lists

---
+++ basics.py
@@ -0,0 +1,56 @@
+from module import function
+
+import module
+
+def func(var1, var2):
+    """
+    Docstring after function def as description
+    :var1: - variable 1
+    :var2: - variable 2
+    """
+    #calculations
+
+    return var
+
+variable_in_snake_case
+
+CONST_IN_SCREAMING_SNAKE_CASE = 50
+
+"""
+and, or, not
+
+int(), float()
++,-,*
+/ divide exactly
+// qoutient
+% reminder
+"""
+
+if a > b:
+    return a
+elif b > a:
+    return b
+else:
+    return None
+
+# List
+fruits = ["apple","Banana"]
+
+# Tuple
+fruits = ('apple', 'banana')
+
+# Set
+fruits = {'apple', 'banana'}
+
+# Dictionary
+names = {1:"cat", 2:'dog'}
+
+1 in names
+#True
+
+1 not in names
+#False
+
+True == 1 in names
+True is 1 in names
+False is not 1 in names
